a0f705a41eb9f3bcf0b947bec868837881e6f2df
Armando <nicoluseche312007@gmail.com>
2026-09-21 20:56:32 -0500

    TrabajoCamara

---
+++ photo-gallery/src/app/services/photo.ts
@@ -0,0 +1,31 @@
+import { Injectable, signal } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoService {
+  public photos = signal<UserPhoto[]>([]);
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+
+    this.photos.update((photos) => [
+      {
+        filepath: 'soon...',
+        webviewPath: capturedPhoto.webPath!,
+      },
+      ...photos,
+    ]);
+  }
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
---
+++ photo-gallery/src/app/tab2/tab2.page.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+} from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { camera } from 'ionicons/icons';
+import { PhotoService } from '../services/photo';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonFab, IonFabButton, IonIcon],
+})
+export class Tab2Page {
+  public photoService = inject(PhotoService);
+
+  constructor() {
+    addIcons({ camera });
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+}
